afe468637e392d92738d36c34b42197b337c2ebc
raj saija <raj.saija@mindinventory.com>
2024-05-20 11:06:12 +0530

    no message

--- VideoPlayer.swift
+++ VideoPlayer.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVFoundation
 import AVKit
 
-public class VideoPlayer: UIViewController {
+public class VideoPlayer: UIView {
 
     public var collectionView: UICollectionView!
    
